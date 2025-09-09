088686d9a2dda1b9b3ec16256442d8053d3f8fa2
이원근 <leewg97@naver.com>
2025-09-09 14:54:19 +0900

    Initial commit

---
+++ src/main/kotlin/com/chakgalpi/chakgalpiapi/ChakgalpiApplication.kt
@@ -0,0 +1,11 @@
+package com.chakgalpi.chakgalpiapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChakgalpiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChakgalpiApplication>(*args)
+}
---
+++ src/test/kotlin/com/chakgalpi/chakgalpiapi/ChakgalpiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chakgalpi.chakgalpiapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChakgalpiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
